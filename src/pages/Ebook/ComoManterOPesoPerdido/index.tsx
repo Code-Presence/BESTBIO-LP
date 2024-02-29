ef201db0f8cc6a64fd94d8ca159bc932baa3052f
Átila de Freitas <atila.defreitas@icloud.com>
2024-02-29 15:55:32 -0300

    setting up bonus section

--- src/pages/Ebook/ComoManterOPesoPerdido/index.tsx
+++ src/pages/Ebook/ComoManterOPesoPerdido/index.tsx
@@ -1,11 +1,13 @@
-import { Typography, Button, Card, CardHeader } from '@material-tailwind/react';
+import { Typography, Button, Card } from '@material-tailwind/react';
 
 import GenteMalhando  from '../../../assets/bg/gente-se-exercitando.webp';
 import GenteNoCampo  from '../../../assets/bg/gentee-no-campo.webp';
-import MulherComendo from '../../../assets/PESSOA-SAUDAVEL.png';
+import MulherComendo from '../../../assets/PESSOA-SAUDAVEL.webp';
 
-import Ebook1  from '../../../assets/Mocks/BookMockup-1.png';
-import Ebook2  from '../../../assets/Mocks/BookMockup-2.png';
+import Ebook1  from '../../../assets/Mocks/BookMockup-1.webp';
+// import Ebook2  from '../../../assets/Mocks/BookMockup-2.webp';
+import BonusEbook1  from '../../../assets/Mocks/BonusBook-1.webp';
+import BonusEbook2  from '../../../assets/Mocks/BonusBook-2.webp';
 
 import Noise from '../../../assets/Textures/nnnoise.svg';
 
@@ -13,7 +15,7 @@ import Aos from 'aos';
 import 'aos/dist/aos.css';
 import React from 'react';
 import { ebook_features } from './features';
-import { Brain, ChevronDown, Lightbulb, Lock, Search } from 'lucide-react';
+import { Brain, ChevronDown, Lightbulb, Search } from 'lucide-react';
 
 function Ebook() {
 
@@ -92,14 +94,12 @@ function Ebook() {
                     A chave para a manutenção do peso é adotar hábitos sustentáveis, em vez de se render a dietas extremas que prometem resultados rápidos. Promovemos uma mudança gradual e realista nos hábitos alimentares e de estilo de vida, focando na saúde a longo prazo e na prevenção do efeito sanfona.
                     </Typography> 
 
-
-
                     <Button className='' size='lg' placeholder={''} color='green' fullWidth data-aos="fade-right" data-aos-delay={400}>Saiba mais</Button>
                 </div>
 
         
                 <div className='w-full h-full flex flex-col items-center justify-center'>
-                    <img src={MulherComendo} className='rounded-lg'/>
+                    <img src={MulherComendo} alt='mulher se alimentando bem' className='rounded-lg saturate-[120%]'/>
                 </div>
             </section>
 
@@ -119,12 +119,13 @@ function Ebook() {
                         </svg>
                     </div>
 
-                    <div className='w-full grid grid-cols-1 lg:grid-cols-2 lg:px-52 gap-12'>
-                        <img src={Ebook1} className='lg:h-[40rem] h-[20rem] object-cover object-center' data-aos="fade-up"/>
+                    <div className='w-full grid grid-cols-1 lg:grid-cols-2 gap-12'>
+                        <div className='w-full h-full flex items-end justify-end'>
+                            <img src={Ebook1} alt='Ebook como manter o peso perdido' className='lg:h-[40rem] h-[20rem] object-cover object-center ml-2' data-aos="fade-up"/>
+                        </div>
                     
-                        <div className=' w-full h-full flex flex-col gap-6 overflow-hidden justify-center pb-12' data-aos="fade-up" data-aos-delay={200}>
+                        <div className=' w-full h-full flex flex-col gap-6 overflow-hidden justify-center pr-24 pb-12' data-aos="fade-up" data-aos-delay={200}>
                             <span>
-
                                 <Typography className='font-bold text-white' variant='lead' placeholder={''} color='black'>
                                   Encontre a chave para uma vida saudável com nosso ebook exclusivo
                                 </Typography>
@@ -165,7 +166,7 @@ function Ebook() {
                     <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 h-fit'>
                         {ebook_features.map((item, index) => (
                             <>
-                                <Card className='p-4 gap-2 flex flex-col items-center' key={index} data-aos="fade-up" data-aos-delay={100 * index}>
+                                <Card className='px-4 pb-4 gap-2 pt-8 flex flex-col items-center' key={index} data-aos="fade-up" data-aos-delay={100 * index}>
                                     <div className=' w-full items-center justify-center flex'>
                                         <div className="h-14 w-14 grid items-center justify-center !rounded-lg bg-[rgba(125,217,86)] text-white">
                                             {item.title == 'Conhecimento especializado ao seu alcance:' && <Brain size={30}/>}
@@ -190,29 +191,105 @@ function Ebook() {
                 </div>
             </section>
 
-            <section className='w-screen h-screen bg-white relative flex flex-col items-center justify-center'>
-                <div
-                    ref={ref}
-                    className={`w-[40rem] lg:flex hidden h-[40rem] from-[rgba(125,217,86)] to-[#b9fa9e] right-96 bg-gradient-to-t absolute rounded-md shadow-xl ${
-                        isVisible ? 'animate-moveAndRotate' : ''
-                    }`}
-                >
-                    <img src={Noise} className=''/>
+            <section className='w-screen h-screen bg-white relative hidden lg:grid grid-cols-2 items-center justify-center gap-4 px-64 py-12'>
+                <div className='w-full h-full justify-center flex flex-col'>
+                    <Typography className='' variant='h2'>Bônus exclusivo</Typography>
+
+                    <Typography>asdas</Typography>
                 </div>
 
-                <div
-                    ref={ref}
-                    className={'w-[30rem] lg:hidden flex h-[30rem] from-[rgba(125,217,86)] to-[#b9fa9e] bg-gradient-to-t absolute rounded-md shadow-xl -right-80 rotate-12'}
-                >
-                    <img src={Noise} className=''/>
+                <div className='w-full h-full flex flex-col items-center justify-center'>
+                    <div
+                        ref={ref}
+                        className={`w-[40rem] lg:flex hidden h-[40rem] from-[rgba(125,217,86)] to-[#b9fa9e] right-96 bg-gradient-to-t absolute rounded-md shadow-xl ${
+                            isVisible ? 'animate-moveAndRotate' : ''
+                        }`}
+                    >
+                        <img src={Noise} alt='just a noise' className=''/>
+                    </div>
+
+                    <img 
+                        className='lg:h-[35rem] h-[25rem] absolute right-10 top-8' src={BonusEbook2} 
+                        alt='ebook saladas e molhos'
+                        style={{ filter: 'drop-shadow(0 10px 20px rgba(0,0,0,0.4))' }}
+                        data-aos="fade-up"
+                        data-aos-duration="1000"
+                    />
+
+                    <img 
+                        className='lg:h-[28rem] h-[25rem] absolute right-72' src={BonusEbook1} 
+                        alt='ebook sobremesas'
+                        style={{ filter: 'drop-shadow(0 10px 20px rgba(0,0,0,0.4))' }}
+                        data-aos="fade-up"
+                        data-aos-duration="1200"
+                        data-aos-delay={200}
+                    />
                 </div>
 
-                <img 
-                    className='lg:h-[45rem] h-[25rem] absolute right-12' src={Ebook2} 
-                    data-aos="flip-left"
-                    data-aos-easing="ease-out-cubic"
-                    data-aos-duration="500"
-                />
+                {/* <div className='absolute bottom-0 -mb-1 bg-gradient-to-b bg- w-full h-[5rem] from-[rgba(255,255,255)] to-[rgba(125,217,86)] flex items-center justify-center'>
+   
+                </div> */}
+            </section>
+
+            <section className='w-scrren h-screen bg-white lg:hidden flex flex-col grid-rows-3 py-12'>
+                <Typography className='ml-4' variant='h2'>Bônus exclusivo</Typography>
+
+                <div className='h-[25rem] w-full pl-4 relative overflow-hidden flex flex-col'>
+                    <div className='w-full bg-white h-[25rem] flex items-center'>
+                        <div className='w-[55%]' data-aos="fade-left" data-aos-delay={200}>
+                            <Typography variant='lead' className='text-justify'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, numquam ipsum? Perspiciatis provident error repudiandae magnam corporis animi.</Typography>
+                        </div>
+                        <img 
+                            src={BonusEbook2}
+                            alt='ebook saladas e molhos'
+                            className='lg:h-[35rem] h-[20rem] absolute -right-20' 
+                            style={{ filter: 'drop-shadow(0 10px 20px rgba(0,0,0,0.4))' }}
+                            data-aos="fade-left"
+                            data-aos-duration="1000"
+                        />
+                    </div>
+                </div>
+
+                <div className='h-[25rem] w-full relative overflow-hidden flex flex-col pr-4 -mt-20'>
+                    <div className='w-full h-[25rem] flex items-center justify-end'>
+                        <img 
+                            src={BonusEbook1}
+                            alt='ebook sobremesas'
+                            className='h-[18rem] absolute -left-16' 
+                            style={{ filter: 'drop-shadow(0 10px 20px rgba(0,0,0,0.4))' }}
+                            data-aos="fade-right"
+                            data-aos-delay={400}
+                            data-aos-duration="1000"
+                        />
+                        <div 
+                            className='w-[55%]'
+                            data-aos="fade-right"
+                            data-aos-delay={600}
+                        >
+                            <Typography variant='lead' className='text-justify'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, numquam ipsum? Perspiciatis provident error repudiandae magnam corporis animi.</Typography>
+                        </div>
+                    </div>
+                </div>
+
+                <Button className='mx-4' size='lg' color='green'>Garantir bônus!</Button>
+                
+
+            </section>
+
+            <section className='w-screen h-screen relative bg-[rgba(125,217,86)] -mt-1 grid grid-cols-1 lg:grid-cols-2 gap-6 px-8 lg:px-64 py-12' >
+                <div className='w-full absolute top-0 flex -mt-1'>
+                    <svg className="w-full h-[14vh] min-h-[100px] max-h-[250px]  rotate-180" xmlns="http://www.w3.org/2000/svg" viewBox="0 20 200 40" preserveAspectRatio="none" shapeRendering="auto">
+                        <defs>
+                            <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+                        </defs>
+                        <g className="parallax">
+                            <use href="#gentle-wave" x="10" y="-6" fill="rgba(255,255,255,0.8)" />
+                            <use href="#gentle-wave" x="55" y="1" fill="rgba(255,255,255,0.6)" />
+                            <use href="#gentle-wave" x="42" y="4" fill="rgba(255,255,255,0.2)" />
+                            <use href="#gentle-wave" x="12" y="10" fill="#fff" />
+                        </g>
+                    </svg>
+                </div>
             </section>
         </>
     );
