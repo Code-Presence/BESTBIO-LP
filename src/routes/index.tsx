fe716d5a0789e91b5bde6316b152242ae5c88083
Átila de Freitas <atila.jcdefreitas@protonmail.com>
2024-12-27 13:55:53 -0300

    minor changes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,73 +1,82 @@
-import React, { Suspense } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { Main } from '../pages/home';
-import { CustomNavbar } from '../components/Navbar';
-import { Presential } from '../pages/presential';
+import React, { Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+import { Main } from "../pages/home";
+import { CustomNavbar } from "../components/Navbar";
+import { Presential } from "../pages/presential";
 
-import { Footer } from '../components/Footer';
-import { Online } from '../pages/online';
-import { PlansWrapper } from '../components/PlansWrapper';
+import { Footer } from "../components/Footer";
+import { Online } from "../pages/online";
+import { PlansWrapper } from "../components/PlansWrapper";
 
-import Aos from 'aos';
-import 'aos/dist/aos.css';
-import { Pro } from '../pages/pro';
-import { Checkout } from '../pages/checkout';
-import { Ebook } from '../pages/Ebook/ComoManterOPesoPerdido';
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { Pro } from "../pages/pro";
+import { Checkout } from "../pages/checkout";
+import { Ebook } from "../pages/Ebook/ComoManterOPesoPerdido";
 
 function AppRoutes(): JSX.Element {
+  React.useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
 
-    React.useEffect(() => {
-        Aos.init({duration: 1000});
-    }, []);
-
-    return (
-        <>
-            <Suspense fallback={
-                <div className='w-screen h-screen bg-[#1b1b1b]'>
-                    <h1>Carregando...</h1>
-                </div>
-            }> 
-                <Routes>
-                    <Route path='/' element={ 
-                        <>
-                            <CustomNavbar isSticky={true} />
-                            <Main /> 
-                        </>
-                    }/>
-                    <Route path='/presencial' element={ 
-                        <>
-                            <CustomNavbar />
-                            <PlansWrapper>
-                                <Presential /> 
-                            </PlansWrapper>
-                        </>
-                    }/>
-                    <Route path='/online' element={
-                        <>
-                            <CustomNavbar />
-                            <PlansWrapper>
-                                <Online />
-                            </PlansWrapper>
-                        </>
-                     
-                    }/>
-                    <Route path='/pro' element={
-                        <>
-                            <section className='sections'>
-                                <Pro />
-                            </section>
-                        </>
-                     
-                    }/>
-                    <Route path='/ebook/como-perder-o-peso-perdido' element={<Ebook />}/>
-                    <Route path='/checkout' element={
-
-                        <Checkout />
-                    }/>
-                </Routes>
-                <Footer />
-            </Suspense>
-        </>
-    );
+  return (
+    <>
+      <Suspense
+        fallback={
+          <div className="w-screen h-screen bg-[#1b1b1b]">
+            <h1>Carregando...</h1>
+          </div>
+        }
+      >
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <CustomNavbar isSticky={true} />
+                <Main />
+              </>
+            }
+          />
+          <Route
+            path="/presencial"
+            element={
+              <>
+                <CustomNavbar />
+                <PlansWrapper>
+                  <Presential />
+                </PlansWrapper>
+              </>
+            }
+          />
+          <Route
+            path="/online"
+            element={
+              <>
+                <CustomNavbar />
+                <PlansWrapper>
+                  <Online />
+                </PlansWrapper>
+              </>
+            }
+          />
+          <Route
+            path="/pro"
+            element={
+              <>
+                <section className="sections">
+                  <Pro />
+                </section>
+              </>
+            }
+          />
+          <Route path="/ebook/como-manter-o-peso-perdido" element={<Ebook />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+        <Footer />
+      </Suspense>
+    </>
+  );
 }
 export { AppRoutes };
+
